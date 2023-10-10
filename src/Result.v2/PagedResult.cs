da54c53980cdd1ab2ab3677cc62172340903ba64
minh.vu <minhvu.box@gmail.com>
2023-10-10 11:41:13 +0700

    fix result

--- src/Result.v2/PagedResult.cs
+++ src/Result.v2/PagedResult.cs
@@ -51,7 +51,7 @@ namespace Zord.Result
 
         public IEnumerable<string> Errors { get; set; } = Array.Empty<string>();
 
-        public IPagedInfo PagedInfo { get; set; }
+        public PagedInfo PagedInfo { get; set; }
 
         public IEnumerable<T> Data { get; set; } = new List<T>();
     }
