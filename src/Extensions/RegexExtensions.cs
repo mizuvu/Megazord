a19ee3b05e3f7793cce186e98c584a753386c660
minh.vu <minhvu.box@gmail.com>
2023-10-26 22:44:59 +0700

    update extensions

---
+++ src/Extensions/RegexExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Zord.Extensions
+{
+    public static class RegexExtensions
+    {
+        /// <summary>
+        ///     Remove Emoji icon from string
+        /// </summary>
+        public static string RemoveEmoji(this string text)
+        {
+            return Regex.Replace(text, @"\p{Cs}", "");
+        }
+    }
+}
